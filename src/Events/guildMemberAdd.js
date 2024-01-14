e6248e6dc221b8b19a9f886429046de1a25f21f6
Dreikam <DreikamCorrea4004@hotmail.com>
2024-01-14 02:34:33 -0300

    guild events axios request

--- src/Events/guildMemberAdd.js
+++ src/Events/guildMemberAdd.js
@@ -1,15 +1,28 @@
-const {GatewayDispatchEvents} = require('@discordjs/core')
+const { axios } = require("../Utils/axios");
+const { GatewayDispatchEvents } = require("@discordjs/core");
 
 module.exports = {
-    name: GatewayDispatchEvents.GuildMemberAdd,
-    run: (client, {data}) => {
-        const newUser = {
-            name: data.user.username,
-            discordId: data.user.id,
-            avatar: data.user.avatar,
-            guildId: data.guild_id
-        }
+  name: GatewayDispatchEvents.GuildMemberAdd,
+  run: async (client, { data }) => {
+    const newUser = {
+      guildId: data.guild_id,
+      member: {
+        name: data.user.username,
+        discordId: data.user.id,
+        avatar: data.user.avatar,
+        bot: data.user.bot
+      },
+    };
 
-        console.log(newUser);
+    try {
+      const { data } = await axios.post(
+        `/guilds/${newUser.guildId}/members/add`,
+        newUser
+      );
+
+      console.log(data.message, newUser);
+    } catch ({response: {data}}) {
+      console.log(data);
     }
-}
+  },
+};
--- src/Events/guildMemberRemove.js
+++ src/Events/guildMemberRemove.js
@@ -1,12 +1,21 @@
 const {GatewayDispatchEvents} = require('@discordjs/core')
+const { axios } = require("../Utils/axios");
 
 module.exports = {
     name: GatewayDispatchEvents.GuildMemberRemove,
-    run: (client, {data}) => {
+    run: async (client, {data}) => {
         const removedUser = {
             discordId: data.user.id,
             guildId: data.guild_id
         }
+
         console.log(removedUser);
+        try {
+            const { data } = await axios.delete(`/guilds/${removedUser.guildId}/members/remove`, {data: removedUser});
+      
+            console.log(data);
+          } catch ({response: {data}}) {
+            console.log(data);
+          }
     }
 }
